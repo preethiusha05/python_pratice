5d9941f7099100358f32b68e83210cbf9b387895
Preethi Usha <ushapreethi05@gmail.com>
2026-02-25 17:36:22 +0530

    Initial Commit

---
+++ Feature_example.py
@@ -0,0 +1,21 @@
+def calculate_grade(marks):
+    if marks >= 90:
+        return "A"
+    elif marks >= 75:
+        return "B"
+    elif marks >= 60:
+        return "C"
+    elif marks >= 40:
+        return "D"
+    else:
+        return "Fail"
+
+student_name = input("Enter student name: ")
+marks = int(input("Enter marks (0–100): "))
+
+grade = calculate_grade(marks)
+
+print("\nStudent Report")
+print("Name:", student_name)
+print("Marks:", marks)
+print("Grade:", grade)
